59e502bbec3fba44b7059830d16e5da45f43c38a
Jonas Rosa <jonasdavidrosa@hotmail.com>
2021-03-22 09:01:45 -0300

    expressoes

--- project javascript/projeto js/teste/expressoes_operadores.js
+++ project javascript/projeto js/teste/expressoes_operadores.js
@@ -8,4 +8,33 @@ Expressões e Operadores
     Ternary
  */
 
-let number
+// Operadores unários
+//Typeof, delete
+
+const person ={
+    name: "Jonas",
+    age: 33,
+
+}
+
+delete  person.age// excluir um elemento
+
+console.log(person)
+
+/*
+Opradores matematicos diferenciado
+resto da divisão %
+let variavvel = 11 % 9
+
+incremento
+let variavel = 0
+increment ++ //esta somando 
+console.log(increment)
+
+
+decremento
+exponencial
+*/
+let variavel = 0
+variavel ++ //esta somando 
+console.log(variavel)
